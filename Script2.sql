2bd8943ba700cc3cd43a461591b1fa1c3f92e8d4
Maria Abdullaeva <mashkoyami@gmail.com>
2022-02-14 13:28:40 +0300

    2.png and Script2.sql modified

--- Script2.sql
+++ Script2.sql
@@ -43,3 +43,29 @@ create table if not exists CollectionOfTrecks (
 	id_treck integer references Treck(id),
 	release_year integer not null
 );
+
+alter table Album 
+	drop column id_musician;
+
+drop table CollectionOfTrecks;
+
+create table if not exists Collection (
+	id serial primary key,
+	name varchar(40) unique not null,
+	id_treck integer references Treck(id),
+	release_year integer not null
+);
+
+alter table Collection 
+	drop column id_treck;
+
+alter table Collection 
+	add column id_treck integer;
+
+create table if not exists TreckCollection (
+	id_treck integer references Treck(id),
+	id_collection integer references Collection(id)
+);
+
+
+
